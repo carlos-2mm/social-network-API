0d8a5dbd39a5667118ec088f80dc455d2e063ecf
Carlos <admin@MacBook-Pro-de-Carlos.local>
2023-11-15 14:47:18 -0500

    feat(userController): create user controller file for handling user-related operations

---
+++ controllers/userController.js
@@ -0,0 +1,79 @@
+const { User } = require('../models');
+
+module.exports = {
+
+    // Get all users
+    async getAllUsers(req, res) {
+        try {
+            const user = await User.find();
+            res.json(user);
+            } catch (err) {
+                console.log(err);
+                return res.status(500).json(err);
+            }
+    
+        },
+    
+        // Get a single user by ID
+        async getSingleUser(req, res){
+            try {
+                const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v')
+                .populate('firneds')
+                .populate('thoughts');
+
+                if (!user) {
+                    return res.status(404).json({ message: 'No user with that ID'})
+                }
+                res.json(user);
+            } catch (err) {
+                console.log(err);
+                return res.status(500).json(err);
+            }
+        },
+
+        // Create a new user
+        async createUser(req, res){
+            try {
+                const user = await User.create(req.body);
+                res.json(user)
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        },
+
+        // Update a user by its ID
+        async updateUser(req, res){
+            try {
+                const user = await User.findOneAndUpdate(
+                    { _id: req.params.userID },
+                    { $addToSet: { user: req.body } },
+                    {runValidators: true, new: true }
+                );
+
+                if (!user) {
+                    return res.status(404).json({ message: 'No user found with that ID :(' });
+                }
+
+                res.json(user);
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        },
+
+        // Delete a user by its ID
+        async deleteUser(req, res){
+            try {
+                const user = await User.findOneAndDelete({_id: req.params.userId })
+
+                if (!user){
+                    return res.status(404).json({ message: ' No such user exist '});
+                }
+
+                res.json({ message: 'User succesfully deleted' });
+            } catch (err) {
+                console.log(err);
+                res.status(500).json(err);
+            }
+        }
+};
