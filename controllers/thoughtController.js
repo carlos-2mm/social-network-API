e8a4b760c9f78756a7ebf7e06247218fae467b5b
Carlos Macias <cimm21187@gmail.com>
2023-11-16 00:21:44 -0500

    feat(thoughtController): add reaction management to thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -81,4 +81,46 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    // Create a reaction to a thought
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+            );
+            if (!thought) {
+            return res
+                .status(404)
+                .json({ message: 'No thought found with this ID!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
+
+    // Remove a reaction from a thought
+    async removeReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+            );
+            if (!thought) {
+            return res
+                .status(404)
+                .json({ message: 'No thought found with this ID!' });
+            }
+            
+            res.json(thought);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
 };
